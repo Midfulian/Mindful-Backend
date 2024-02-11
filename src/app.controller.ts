795662ff1122056a6deb534c006069b09ef1999f
ZodicSlanser <zodic.slanser68@yahoo.com>
2024-02-11 18:44:20 +0200

    clean-up

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,5 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
-import { RegisterUserEntity } from './entites/register.user.entity';
-import { LoginUserEntity } from './entites/login.user.entity';
+import { Controller} from '@nestjs/common';
+
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  // @UseGuards(AuthGuard('jwt'))
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
-
-  // @Post('/signin')
-  // signIn(@Body() body: LoginUserEntity) {
-  //   return this.appService.signIn(body);
-  // }
-
-  // @Post()
-  // signUp(@Body() body: RegisterUserEntity) {
-  //   return this.appService.signUp(body);
-  // }
-}
+export class AppController {}
